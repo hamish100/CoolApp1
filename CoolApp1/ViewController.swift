932a3048e3737209aa197bd9a1d47a969470e674
hamish macmillan <hcmac@bigpond.com>
2016-08-22 15:36:08 +1000

    Initial Commit

---
+++ CoolApp1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoolApp1
+//
+//  Created by hamish macmillan on 22/08/2016.
+//  Copyright © 2016 hamish macmillan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
